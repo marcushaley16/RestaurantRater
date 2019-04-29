deb6ce43903da17feaf7d4c1b2f238314dc6f0fe
marcushaley16 <marcushaley16@gmail.com>
2019-04-29 11:59:29 -0400

    Added Restaurant Index View

--- RestaurantRaterMVC/Models/Restaurant.cs
+++ RestaurantRaterMVC/Models/Restaurant.cs
@@ -9,9 +9,9 @@ namespace RestaurantRaterMVC.Models
     public class Restaurant
     {
         public int RestaurantID { get; set; }
-        public string RestaurantName { get; set; }
-        public string RestaurantAddress { get; set; }
-        public int RestaurantRating { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
     }
 
     public class RestaurantDBContext : DbContext
